4e3c9122bc300d23bc1d5e1bd6c30fdf61979515
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2022-05-29 09:22:59 +0300

    (feat) rope auth to client side

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,30 @@
+import { getProviders, signIn } from "next-auth/react";
 import React from "react";
 
-function Login() {
-  return <div>Login</div>;
+function Login({ providers }) {
+  return (
+    <div className="flex flex-col items-center min-h-screen bg-black w-full justify-center">
+      <img className="w-52 mb-5" src="https://links.papareact.com/9xl" alt="" />
+      {providers &&
+        Object.values(providers).map((provider) => (
+          <button
+            className="bg-[#18D860] text-white p-5 rounded-full"
+            key={provider.name}
+            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+          >{`Login with ${provider.name}`}</button>
+        ))}
+    </div>
+  );
 }
 
 export default Login;
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+
+  return {
+    props: {
+      providers,
+    },
+  };
+}
