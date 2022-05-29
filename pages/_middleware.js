64d6fe547c9eab76a9d7bce98aef21e928c8bb5c
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2022-05-29 11:51:02 +0300

    (feat) add middleware + protect auth

---
+++ pages/_middleware.js
@@ -0,0 +1,19 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+
+export async function middleware(req) {
+  // Token will exist if user is logged in
+  const token = await getToken({ req, secret: process.env.JWT_SECRET });
+
+  // Allow requests if user is logged in or a request for next-auth session
+  const { pathname } = req.nextUrl;
+
+  if (pathname.includes("/api/auth") || token) {
+    return NextResponse.next();
+  }
+
+  // Redirect to login if unauthorised and requesting unprotected route
+  if (!token && pathname !== "/login") {
+    return NextResponse.redirect("/login");
+  }
+}
